1a6ff342a03b77b8f8e1bdce79216924d952356a
Eloho23 <odiokosophia@gmail.com>
2023-06-01 15:10:39 -0700

    addnode

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,35 +1,33 @@
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
 /**
 
-add_node - Adds a new node at the beginning of a list_t list.
-@head: A double pointer to the head of the list.
-@str: The string to be duplicated and added to the list.
-Return: The address of the new element, or NULL if it failed.
-    (On success, returns the address of the new node)
+add_node - Adds a new node at the beginning of a linked list
+
+@head: Double pointer to the list_t list
+
+@str: New string to add in the node
+
+Return: The address of the new element, or NULL if it fails
 */
 list_t *add_node(list_t **head, const char *str)
 {
 list_t *new_node;
 unsigned int len = 0;
 
-python
-Copy code
+while (str[len])
+len++;
+
 new_node = malloc(sizeof(list_t));
-if (new_node == NULL)
-	return (NULL);
+if (!new_node)
+return (NULL);
 
 new_node->str = strdup(str);
-if (new_node->str == NULL)
-{
-	free(new_node);
-	return (NULL);
-}
-
-while (str[len] != '\0')
-	len++;
-
 new_node->len = len;
-new_node->next = *head;
-*head = new_node;
+new_node->next = (*head);
+(*head) = new_node;
 
-return (new_node);
+return (*head);
 }
