980915855ebb9be70fe62a945f08106500348846
Eloho23 <odiokosophia@gmail.com>
2023-06-06 03:18:22 -0700

    k

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++
@@ -1,9 +0,0 @@
-#include "lists.h"
-size_t print_listint(const listint_t *h) {
-  size_t n = 0;
-  while (h != NULL) {
-    printf("element %zu: %d\n", n++, h->n);
-    h = h->next;
-  }
-  return n;
-}
